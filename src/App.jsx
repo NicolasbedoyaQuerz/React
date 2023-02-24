a3b60c3715abe1913f3d9498631f1c1f3701672f
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-02-23 20:10:48 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,11 @@ import "./App.css";
 import Button from "./components/Button";
 import CardSong from "./components/CardSong";
 
-function App() {
-  /*
-  function addNumbers(a, b){
-    return a + b
-  }
-
-  addNumber( 5, 10 )
-  addNuumber( 100, 200 )
-*/
+function App() {  
+  
   return (
     <div className="App">
-      <h1> {100 + 100} </h1>
-      <Button propiedad="Este es el valor de la propiedad" texto="Cancelar" />
-      <Button texto="Aceptar" />
-      <Button texto="Iniciar sesión" />
 
-      <CardSong
-        title="Storytime"
-        album="Imaginaerum"
-        band="Nightwish"
-        release="2011"
-        genre="Metal sinfonico"
-      />
-      <CardSong
-        title="Flight of Icarus"
-        album="Piece of mind"
-        band="Iron Maiden"
-        release="1983"
-        genre="Heavy Metal"
-      />
     </div>
   );
 }

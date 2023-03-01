df30b336e1453abd379e1301bbd8403a73ac82cd
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-02-28 20:14:28 -0500

    febrero-27

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import Title from "./components/Title";
+
 function App() {  
-  
+  const [value, setValue] = useState('');
+  const [isPasswordShown, setPasswordShown] = useState(false);
+
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(!isPasswordShown);
+  };
+
   return (
     <div className="App">
-
+      <Title text={value}></Title>
+      <label htmlFor="password">Contraseña</label>
+      <input 
+        id="password"
+        type={isPasswordShown ? "text" : "password"} 
+        onChange={(e) => setValue(e.target.value)} />
+      <button onClick={togglePasswordVisiblity}>
+        {isPasswordShown ? "Ocultar" : "Mostrar"}
+      </button>
     </div>
   );
 }

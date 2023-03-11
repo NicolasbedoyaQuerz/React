8b34ce323c06ddcea6dfaab55199d7f498847ab2
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-03-10 23:00:45 -0500

    api de form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,63 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const ToDoForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [isCompleted, setIsCompleted] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const todo = {
-      id: Date.now(),
-      title: title,
-      description: description,
-      isCompleted: isCompleted,
-    };
-    console.log(todo);
-
-    reset()
+  const onSubmit = (data) => {
+    console.log(data);
+    resetForm();
   };
 
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
+  const resetForm = () => {
+    reset({
+      title: "",
+      description: "",
+      isCompleted: false,
+    });
   };
 
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
+  const handleReset = () => {
+    resetForm();
   };
 
-  const handleIsCompletedChange = (event) => {
-    setIsCompleted(event.target.checked);
+  const handleFillForm = () => {
+    reset({
+      title: "Título de ejemplo",
+      description: "Descripción de ejemplo",
+      isCompleted: true,
+    });
   };
 
-  
-  const reset = () => {
-    setTitle('')
-    setDescription('')
-    setIsCompleted(false)
-
-    alert('Formulario enviado!')
-  }
   return (
-    <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", alignItems: "center", border: "0.5px solid black", padding: "20px", borderRadius: "5px", boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)", width: "80%", margin: "0 auto" }}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        border: "0.5px solid black",
+        padding: "20px",
+        borderRadius: "5px",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+        width: "80%",
+        margin: "0 auto",
+      }}
+    >
       <div>
         <h1>Nueva Tarea</h1>
       </div>
       <div>
         <label htmlFor="title">Título</label>
-        <input type="text" id="title" value={title} onChange={handleTitleChange} required/>
+        <input type="text" id="title" {...register("title", { required: true })} />
+        {errors.title?.type === "required" && (
+          <p role="alert">El título es obligatorio</p>
+        )}
       </div>
       <div>
         <label htmlFor="description">Descripción</label>
-        <textarea id="description" value={description} onChange={handleDescriptionChange}></textarea>
+        <textarea id="description" {...register("description", { required: true })} />
+        {errors.description?.type === "required" && (
+          <p role="alert">La descripción es obligatoria</p>
+        )}
       </div>
       <div>
         <label htmlFor="isCompleted">Completado</label>
-        <input type="checkbox" id="isCompleted" checked={isCompleted} onChange={handleIsCompletedChange} required/>
+        <input type="checkbox" id="isCompleted" {...register("isCompleted")} />
       </div>
-      <button type="submit">Enviar</button>
+      <div>
+        <button type="button" onClick={handleFillForm}>
+          Rellenar Formulario
+        </button>
+        <button type="button" onClick={handleReset}>
+          Limpiar Formulario
+        </button>
+        <button type="submit">Enviar</button>
+      </div>
     </form>
   );
 };

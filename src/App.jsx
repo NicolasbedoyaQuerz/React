3af1cc16df41de978578e64547f8572d37cdc8bf
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-04-05 18:49:30 -0500

    api de pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import "./App.css";
 import Character from "./components/Character";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 
 function App() {  
+
+  const [ characterData, setCharacterData] = useState({})
+  const [ randomPokemon, setRandomPokemon] = useState( 1 )
+
+  useEffect(() => {
+
+    axios 
+      .get(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+      .then(resp => setCharacterData(resp.data))
+      .catch(error => console.log(error.data))
+  }, [randomPokemon])
+
+
+  const generateRandomNumber = () => {
+    const numberRandom = Math.floor(Math.random(1) * 800)
+    setRandomPokemon(numberRandom)
+  }
+
+
   
   return (
     <div className="App">
-      <Character idOrName="55" />
+      <Character
+      data = { characterData }
+      />
+       <button onClick={generateRandomNumber}>Change Pokemon</button>
     </div>
   );
 };
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,62 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from "react";
 
-const Character = ({ idOrName }) => {
-  const [character, setCharacter] = useState(null);
-  const [unit, setUnit] = useState('m');
+const Character = ({data}) => {
 
-  const getRandomPokemon = () => {
-    const randomId = Math.floor(Math.random() * 151) + 1;
-    setCharacter(null);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
-      .then(response => response.json())
-      .then(data => setCharacter(data))
-      .catch(error => console.error(error));
-  };
+  const [isDecimeters, setIsDecimeters] = useState(true)
 
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`)
-      .then(response => response.json())
-      .then(data => setCharacter(data))
-      .catch(error => console.error(error));
-  }, [idOrName]);
-
-  const convertHeight = (height) => {
-    switch (unit) {
-      case 'cm':
-        return height * 10;
-      case 'm':
-        return height / 10;
-      case 'dm':
-        return height;
-      default:
-        return height;
-    }
-  };
-
+ 
+  
   return (
-    <div>
-      {character ? (
-        <div>
-          <h1>{character.name}</h1>
-          <img src={character.sprites.front_default} alt={character.name} />
-          <h3><span>Weight:</span> {character.weight} lbs</h3>
-          <h3><span>Height:</span> {convertHeight(character.height)} {unit}</h3>
-          <h3><span>Type:</span> {character.types.map(type => type.type.name).join(', ')}</h3>
-          <button onClick={getRandomPokemon}>Get random Pokémon</button>
-          <div>
-            <label>
-              Height unit:{' '}
-              <select value={unit} onChange={(event) => setUnit(event.target.value)}>
-                <option value="m">m</option>
-                <option value="dm">dm</option>
-                <option value="cm">cm</option>
-              </select>
-            </label>
-          </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="character-card">
+      <h1>{data.forms?.[0].name}</h1>
+      <img src={data.sprites?.front_default} alt="" />
+      <h3><span>Weight:</span> {data.weight} </h3>
+      <h3><span>Height:</span> {data.height} </h3>
+      <h3><span>Type:</span> {data.types?.[0].type?.name}</h3>
+      <h3><span>Altura:</span> { isDecimeters ? `${data.height} ` : data.height / 10  } { isDecimeters ? 'decimeters' : 'metros'}</h3>
+      
+      <button onClick={() => setIsDecimeters(!isDecimeters)}>Cambiar altura</button>
     </div>
   );
 };

f3093aaaad704c15290d833a4559a6ccc4ff018d
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-04-05 01:13:25 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import "./App.css";
-import react from './assets/react.svg'
-import UserCard from "./components/UserCard";
-import users from './data/users.json'
-import {useState} from 'react'
-function App() {  
-  const colors = ["#FDB137", "#785964", "#6D6875", "#B5838D", "#E5989B", "#7E9680", "#C73866", "#FFB4A2", "#79616F", "#EAB595" ]
-  const [ index, setIndex ] = useState(0)
-  const changeUser = () => {
-    const randomIndex = Math.floor(Math.random() * users.length);
-    setIndex(randomIndex);
-  }
-  //   if (index === (users.length -1)) {
-  //     setIndex(0);
-  //   } else {
-  //     setIndex(index + 1);
-  //   }
-  // }
-  
-  return (
-    <div className="App" style={{backgroundColor: colors[index]}}>
-      {/* <img src="./vite.svg" /> 
-      <img src={react} />*/}
+import Button from "./components/Button";
+import CardSong from "./components/CardSong";
 
-      <UserCard
-      userData = {users[index]}
+function App() {
+  /*
+  function addNumbers(a, b){
+    return a + b
+  }
+
+  addNumber( 5, 10 )
+  addNuumber( 100, 200 )
+*/
+  return (
+    <div className="App">
+      <h1> {100 + 100} </h1>
+      <Button propiedad="Este es el valor de la propiedad" texto="Cancelar" />
+      <Button texto="Aceptar" />
+      <Button texto="Iniciar sesión" />
+
+      <CardSong
+        title="Storytime"
+        album="Imaginaerum"
+        band="Nightwish"
+        release="2011"
+        genre="Metal sinfonico"
       />
-      
-      <button onClick={changeUser}><i class='bx bx-shuffle' ></i></button>
+      <CardSong
+        title="Flight of Icarus"
+        album="Piece of mind"
+        band="Iron Maiden"
+        release="1983"
+        genre="Heavy Metal"
+      />
     </div>
   );
 }
 
 export default App;
+ 

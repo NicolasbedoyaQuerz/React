296d159d5554c24e63d0d73d7a2c1f50f5ded0b0
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-04-08 14:16:51 -0500

    repaso de hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,40 +1,56 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
+const Form = ({ createTodo }) => {
 
-const Form = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm()
 
-    const {register, handleSubmit} = useForm()
-
-    const submit = data => {
-        console.log(data);
+    const onSubmit = data => {
+        data.id = Date.now()
+        data.isCompleted = false
+        createTodo(data)
+        emptyForm()
     }
 
-  
+    const fillForm = () => {
+        reset(
+            {
+                title: "Estudiar React",
+                description: "Estudiar useState y useEffect",
+                isCompleted: false,
+                id: 1
+            }
+        )
+    }
+
+    const emptyForm = () => {
+        reset(
+            {
+                title: "",
+                description: "",
+                isCompleted: false,
+                id: ""
+            }
+        )
+    }
+
     return (
-        <form onSubmit={handleSubmit( submit )}>
-            <div>
-                <label htmlFor="username"> Nombre de Usuario </label>
-                <input 
-                type="text" 
-                name="username" 
-                id="username" 
-                placeholder="UserName"
-                {...register('username')}
-                />
-            </div>
-            <div>
-                <label htmlFor="password"> Contraseña </label>
-                <input 
-                type="password" 
-                name="password" 
-                id="password" 
-                placeholder="Eje.. 1234"
-                {...register('password')}
-                />
-            </div>
-            <button type="submit"> Iniciar Sesion </button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="title">Título</label>
+                    <input type="text" id="title" {...register("title", { required: true })} />
+                    {errors.title?.type === 'required' && <p role="alert" style={{ color: 'tomato' }}>El título es requerido</p>}
+                </div>
+                <div>
+                    <label htmlFor="description">Descripción</label>
+                    <input type="text" id="description" {...register("description", { required: true })} />
+                    {errors.description?.type === 'required' && <p role="alert" style={{ color: 'tomato' }}>La descripción es requerida</p>}
+                </div>
+                <button type="submit">Agregar ToDo</button>
+            </form>
+            {/* <button onClick={fillForm}>Llenar formulario</button> */}
+        </div>
     );
-};
+}
 
 export default Form;
